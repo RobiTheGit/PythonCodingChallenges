cc84079ddd1415172886f2816b1fe7c340f47b68
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-07-27 10:45:35 -0600

    Create SumIntFromString.py

---
+++ SumIntFromString/SumIntFromString.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import re
+def IntToString(String):
+    sums = 0
+    String = re.sub(r"[^0-9]", " ", String)
+    for i in String:
+        try:
+            i = int(i)
+            sums += i
+        except:
+            pass
+    print(sums)
+
+IntToString(input('String: '))
