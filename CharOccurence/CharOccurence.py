e8f6968c05e845857c60da3e10f534a27e594e1b
RobiWanKenobi <94720060+RobiTheGit@users.noreply.github.com>
2023-07-28 10:19:03 -0600

    Create CharOccurence.py

---
+++ CharOccurence/CharOccurence.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import re
+def CharacterOccurences(string, char):
+    x = 0
+    for i in string:
+        if i == char:
+            x += 1
+        else:
+            pass
+    print(f'Ocuurences of {char} in {string} =', x)
+    
+CharacterOccurences(input('Input a string: '),input('What letter would you like to find the ammount of: '))
